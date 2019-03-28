f2b92a4e1462316e77e21ec263c31947d662282c
Luis Serra <serra.anonym0us@gmail.com>
2019-03-28 13:44:11 +0900

    Primer commit
    Se han subido las dos clases necesarias para realizar los RSA

---
+++ src/RSAEncrypter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RSAEncryption
+{
+    public static class RSAEncrypter
+    {
+        /// <summary>
+        /// Metodo de encriptación del texto
+        /// </summary>
+        /// <param name="text">Texto a encriptar</param>
+        /// <param name="xmlPublic">Clave púplica</param>
+        /// <returns></returns>
+        public static byte[] Encrypt(string text, string xmlPublic)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(1024);
+            RSA.FromXmlString(xmlPublic);
+            byte[] encryptedData = RSA.Encrypt(Encoding.UTF8.GetBytes(text), false);
+            return encryptedData;
+        }
+
+        /// <summary>
+        /// Metodo de desencriptación de texto
+        /// </summary>
+        /// <param name="encryptedText">Texto encriptado</param>
+        /// <param name="xmlPrivate">Clave privada</param>
+        /// <returns></returns>
+        public static byte[] Decrypt(string encryptedText, string xmlPrivate)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(1024);
+            RSA.FromXmlString(xmlPrivate);
+            byte[] decryptData = RSA.Decrypt(Convert.FromBase64String(encryptedText), false);
+            return decryptData;
+        }
+    }
+}
---
+++ src/RSAProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace RSAEncryption
+{
+    public class RSAProvider
+    {
+        public RSACryptoServiceProvider RSAService { get; set; }
+
+        public RSAProvider()
+        {
+            RSAService = new RSACryptoServiceProvider();
+        }
+
+        public byte[] CreatePublicKey()
+        {
+            string xmlPublicKey = RSAService.ToXmlString(false);
+            return Encoding.ASCII.GetBytes(xmlPublicKey);
+        }
+
+        public byte[] CreatePrivateKey()
+        {
+            string xmlPrivateKey = RSAService.ToXmlString(true);
+            return Encoding.ASCII.GetBytes(xmlPrivateKey);
+        }
+    }
+}
